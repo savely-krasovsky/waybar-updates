cfa89fe5e933849fbcd2095047763d238dceb6a6
ashish-kus <ashish.kus2408@gmail.com>
2024-10-13 18:29:44 +0530

    improving

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=$(gettext "waybar-updates" "Name")
+old_version=$(gettext "waybar-updates" "Old Version")
+new_version=$(gettext "waybar-updates" "New Version")
+
+echo "Name: $name"
+echo "Old Version: $old_version"
+echo "New Version: $new_version"
